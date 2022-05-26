b9658e1ea6c2f76e41b31f1088e7d7f0c6828aa5
Sergio Lopez <slp@redhat.com>
2022-05-26 19:03:41 +0200

    Update to TSIv2 and 5.15.43
    Update to the latest LTS available (5.15.43) and replace the patchset
    with the one implementing TSIv2.
    
    This commit also bumps the internal version from "1" to "2", as the
    TSIv2 change breaks compatibility with older libkrun versions.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- Image_to_bundle.py
+++ Image_to_bundle.py
@@ -10,11 +10,11 @@ char * krunfw_get_kernel(size_t *load_addr, size_t *size)
 
 int krunfw_get_version()
 {
-    return 1;
+    return 2;
 }
 """
 
-load_segments = [ ]    
+load_segments = [ ]
 
 if len(sys.argv) != 2:
     print('Invalid arguments')
@@ -41,7 +41,7 @@ while byte != b"":
         col = col + 1
     byte = kelf.read(1)
     pos = pos + 1
-            
+
 kc.write('";\n')
 kc.write('size_t KERNEL_SIZE = 0x%s;\n' % format(pos, 'x'))
 kc.write('size_t KERNEL_LOAD_ADDR = 0x%s;\n' % format(entry, 'x'))
