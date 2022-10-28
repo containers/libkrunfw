cf1eaedcb2dd8a6bde00bc49f4e0476b4ec0ef17
Sergio Lopez <slp@redhat.com>
2022-10-28 19:34:13 +0200

    sev: Calculate SNP launch measurement
    Extend krunfw_measurement to, in addition of calculating the
    measurement of SEV and SEV-ES, do it so for SNP too.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- utils/krunfw_measurement.c
+++ utils/krunfw_measurement.c
@@ -25,9 +25,24 @@
 #define KRUN_RAMDISK_ADDR       0xa00000
 #define KRUN_RAMDISK_SIZE       0x19e000
 
-char * (*krunfw_get_kernel) (size_t *load_addr, size_t *size);
-char * (*krunfw_get_initrd) (size_t *size);
-char * (*krunfw_get_qboot) (size_t *size);
+struct page_info
+{
+	uint8_t current[48];
+	uint8_t contents[48];
+	uint16_t length;
+	uint8_t page_type;
+	uint8_t imi_page : 1; // bit 0
+	uint8_t reserved : 7; // bits 1 to 7
+	uint8_t resv;
+	uint8_t vmpl_1_perms;
+	uint8_t vmpl_2_perms;
+	uint8_t vmpl_3_perms;
+	uint64_t gpa;
+} __attribute__((__packed__));
+
+char *(*krunfw_get_kernel)(size_t *load_addr, size_t *size);
+char *(*krunfw_get_initrd)(size_t *size);
+char *(*krunfw_get_qboot)(size_t *size);
 
 struct boot_params bootp;
 
@@ -53,24 +68,120 @@ void setup_bootp(int num_cpus, int ram_mib)
 	bootp.e820_table[0].size = EBDA_START;
 	bootp.e820_table[0].type = 1;
 
-	mem_size = ((unsigned long long) ram_mib) * 1024 * 1024;
-	if (mem_size <= MMIO_MEM_START) {
+	mem_size = ((unsigned long long)ram_mib) * 1024 * 1024;
+	if (mem_size <= MMIO_MEM_START)
+	{
 		bootp.e820_table[1].addr = HIMEM_START;
 		bootp.e820_table[1].size = mem_size - HIMEM_START + 1;
 		bootp.e820_table[1].type = 1;
 
-                bootp.e820_entries = 2;
-        } else {
-                bootp.e820_table[1].addr = HIMEM_START;
-                bootp.e820_table[1].size = MMIO_MEM_START - HIMEM_START;
-                bootp.e820_table[1].type = 1;
+		bootp.e820_entries = 2;
+	}
+	else
+	{
+		bootp.e820_table[1].addr = HIMEM_START;
+		bootp.e820_table[1].size = MMIO_MEM_START - HIMEM_START;
+		bootp.e820_table[1].type = 1;
 
-                bootp.e820_table[2].addr = FIRST_ADDR_PAST_32BITS;
-                bootp.e820_table[2].size = mem_size - MMIO_MEM_START + 1;
-                bootp.e820_table[2].type = 1;
+		bootp.e820_table[2].addr = FIRST_ADDR_PAST_32BITS;
+		bootp.e820_table[2].size = mem_size - MMIO_MEM_START + 1;
+		bootp.e820_table[2].type = 1;
 
-                bootp.e820_entries = 3;
-        }
+		bootp.e820_entries = 3;
+	}
+}
+
+void digest_blob(char *current, char *blob, uint64_t load_addr, size_t size)
+{
+	int i, j;
+	SHA512_CTX ctx;
+	struct page_info info;
+
+
+	for (i = 0; i < size; i += 4096)
+	{
+		memset(&info, 0, sizeof(struct page_info));
+
+		memcpy(&info.current[0], current, 48);
+		SHA384(blob + i, 4096, &info.contents[0]);
+
+		info.length = sizeof(struct page_info);
+		info.page_type = 1;
+		info.gpa = load_addr + i;
+
+		SHA384((char *)&info, sizeof(struct page_info), current);
+	}
+}
+
+void digest_zero(char *current, uint64_t gaddr, size_t size, uint8_t page_type)
+{
+	SHA512_CTX ctx;
+	struct page_info info;
+	int i;
+
+	for (i = 0; i < size; i += 4096)
+	{
+		memset(&info, 0, sizeof(struct page_info));
+		memcpy(&info.current[0], current, 48);
+		memset(&info.contents[0], 0, 48);
+		info.length = sizeof(struct page_info);
+		info.page_type = page_type;
+		info.gpa = gaddr + i;
+
+		SHA384((char *)&info, sizeof(struct page_info), current);
+	}
+}
+
+void digest_vmsa(char *current)
+{
+	SHA512_CTX ctx;
+	struct page_info info;
+
+	memset(&info, 0, sizeof(struct page_info));
+	memcpy(&info.current[0], current, 48);
+	SHA384((char *)&SNP_VMSA_BP[0], 4096, &info.contents[0]);
+
+	info.length = sizeof(struct page_info);
+	info.page_type = 2;
+	info.gpa = 0xFFFFFFFFF000;
+
+	SHA384((char *)&info, sizeof(struct page_info), current);
+}
+
+void measurement_snp(int num_cpus)
+{
+	char *payload_addr;
+	size_t payload_size;
+	size_t load_addr;
+	char digest[48] = {0};
+	int i;
+	int ret;
+
+	payload_addr = krunfw_get_qboot(&payload_size);
+	digest_blob(&digest[0], payload_addr, 0xffff0000, payload_size);
+
+	payload_addr = krunfw_get_kernel(&load_addr, &payload_size);
+	digest_blob(&digest[0], payload_addr, load_addr, payload_size);
+
+	payload_addr = krunfw_get_initrd(&payload_size);
+	digest_blob(&digest[0], payload_addr, 0xa00000, payload_size);
+
+	bootp.hdr.ramdisk_size = payload_size;
+	digest_blob(&digest[0], (char *)&bootp, 0x7000, 0x1000);
+
+	digest_zero(&digest[0], 0x0, 0x1000, 4);
+	digest_zero(&digest[0], 0x5000, 0x1000, 5);
+	digest_zero(&digest[0], 0x6000, 0x1000, 6);
+	digest_zero(&digest[0], 0x8000, 0x7000, 4);
+
+	digest_vmsa(&digest[0]);
+
+	printf("SNP:\t");
+	for (i = 0; i < 48; ++i)
+	{
+		printf("%02hhx", digest[i]);
+	}
+	printf("\n");
 }
 
 void measurement_sev_es(int num_cpus)
@@ -97,14 +208,16 @@ void measurement_sev_es(int num_cpus)
 	SHA256_Update(&shactx, &bootp, 4096);
 
 	SHA256_Update(&shactx, &VMSA_BP, 4096);
-	for (i = 1; i < num_cpus; i++) {
+	for (i = 1; i < num_cpus; i++)
+	{
 		SHA256_Update(&shactx, &VMSA_AP, 4096);
 	}
 
 	SHA256_Final(&digest[0], &shactx);
 
 	printf("SEV-ES:\t");
-	for (i = 0; i < 32; ++i) {
+	for (i = 0; i < 32; ++i)
+	{
 		printf("%02lx", digest[i] & 0xFFl);
 	}
 	printf("\n");
@@ -136,7 +249,8 @@ void measurement_sev()
 	SHA256_Final(&digest[0], &shactx);
 
 	printf("SEV:\t");
-	for (i = 0; i < 32; ++i) {
+	for (i = 0; i < 32; ++i)
+	{
 		printf("%02lx", digest[i] & 0xFFl);
 	}
 	printf("\n");
@@ -150,60 +264,70 @@ int main(int argc, char **argv)
 	int num_cpus = 1;
 	int ram_mib = 2048;
 
-	while((opt = getopt(argc, argv, ":c:m:")) != -1)
+	while ((opt = getopt(argc, argv, ":c:m:")) != -1)
+	{
+		switch (opt)
 		{
-			switch(opt)
-				{
-				case 'c':
-					if ((num_cpus = atoi(optarg)) == 0) {
-						printf("Invalid number of CPUs\n");
-					}
-					break;
-				case 'm':
-					if ((ram_mib = atoi(optarg)) == 0) {
-						printf("Invalid amount of RAM\n");
-					}
-					break;
-				}
+		case 'c':
+			if ((num_cpus = atoi(optarg)) == 0)
+			{
+				printf("Invalid number of CPUs\n");
+			}
+			break;
+		case 'm':
+			if ((ram_mib = atoi(optarg)) == 0)
+			{
+				printf("Invalid amount of RAM\n");
+			}
+			break;
 		}
+	}
 
 	library = NULL;
-	if (optind >= argc) {
+	if (optind >= argc)
+	{
 		printf("Usage: %s [-c NUM_CPUS] [-m RAM_MIB] LIBKRUNFW_SO\n", argv[0]);
 		exit(-1);
-	} else {
+	}
+	else
+	{
 		library = argv[optind];
 	}
 
 	handle = dlopen(library, RTLD_NOW);
-	if (handle == NULL) {
+	if (handle == NULL)
+	{
 		perror("Couldn't open library");
 		exit(-1);
 	}
 
 	krunfw_get_kernel = dlsym(handle, "krunfw_get_kernel");
-	if (krunfw_get_kernel == NULL) {
+	if (krunfw_get_kernel == NULL)
+	{
 		perror("Couldn't find krunfw_get_kernel symbol");
 		exit(-1);
 	}
 
 	krunfw_get_initrd = dlsym(handle, "krunfw_get_initrd");
-	if (krunfw_get_initrd == NULL) {
+	if (krunfw_get_initrd == NULL)
+	{
 		perror("Couldn't find krunfw_get_initrd symbol");
 		exit(-1);
 	}
 
 	krunfw_get_qboot = dlsym(handle, "krunfw_get_qboot");
-	if (krunfw_get_qboot == NULL) {
+	if (krunfw_get_qboot == NULL)
+	{
 		perror("Couldn't find krunfw_get_qboot symbol");
 		exit(-1);
 	}
 
 	setup_bootp(num_cpus, ram_mib);
 	printf("Measurements for %d vCPU(s) and %d MB of RAM\n",
-	       num_cpus, ram_mib);
+		   num_cpus, ram_mib);
 	measurement_sev();
 	measurement_sev_es(num_cpus);
+	measurement_snp(num_cpus);
 
 	return 0;
 }
